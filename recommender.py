f661525de9da2706c1f0c6ceb9e5e658b7c10d7e
Yat-Mac <yatingupta7777@gmail.com>
2018-10-19 09:19:06 +0530

    +CRF and hybrid

--- recommender.py
+++ recommender.py
@@ -353,4 +353,141 @@ NUMBER_OF_FACTORS_MF = 15
 U, sigma, Vt = svds(users_items_pivot_matrix, k = NUMBER_OF_FACTORS_MF)
 sigma = np.diag(sigma)
 all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) 
-all_user_predicted_ratings
+#all_user_predicted_ratings
+
+#Converting the reconstructed matrix back to a Pandas dataframe
+cf_preds_df = pd.DataFrame(all_user_predicted_ratings, columns = users_items_pivot_matrix_df.columns, index=users_ids).transpose()
+#cf_preds_df.head(10)
+
+class CFRecommender:
+    
+    MODEL_NAME = 'Collaborative Filtering'
+    
+    def __init__(self, cf_predictions_df, items_df=None):
+        self.cf_predictions_df = cf_predictions_df
+        self.items_df = items_df
+        
+    def get_model_name(self):
+        return self.MODEL_NAME
+        
+    def recommend_items(self, user_id, items_to_ignore=[], topn=10, verbose=False):
+        # Get and sort the user's predictions
+        sorted_user_predictions = self.cf_predictions_df[user_id].sort_values(ascending=False) \
+                                    .reset_index().rename(columns={user_id: 'recStrength'})
+
+        # Recommend the highest predicted rating movies that the user hasn't seen yet.
+        recommendations_df = sorted_user_predictions[~sorted_user_predictions['contentId'].isin(items_to_ignore)] \
+                               .sort_values('recStrength', ascending = False) \
+                               .head(topn)
+
+        if verbose:
+            if self.items_df is None:
+                raise Exception('"items_df" is required in verbose mode')
+
+            recommendations_df = recommendations_df.merge(self.items_df, how = 'left', 
+                                                          left_on = 'contentId', 
+                                                          right_on = 'contentId')[['recStrength', 'contentId', 'title', 'url', 'lang']]
+
+
+        return recommendations_df
+    
+cf_recommender_model = CFRecommender(cf_preds_df, articles_df)
+
+
+'''Evaluating CF model'''
+#print('Evaluating Collaborative Filtering (SVD Matrix Factorization) model...')
+cf_global_metrics, cf_detailed_results_df = model_evaluator.evaluate_model(cf_recommender_model)
+#print('\nGlobal metrics:\n%s' % cf_global_metrics)
+#cf_detailed_results_df.head(10)
+
+'''Hybrid Recommender'''
+class HybridRecommender:
+    
+    MODEL_NAME = 'Hybrid'
+    
+    def __init__(self, cb_rec_model, cf_rec_model, items_df):
+        self.cb_rec_model = cb_rec_model
+        self.cf_rec_model = cf_rec_model
+        self.items_df = items_df
+        
+    def get_model_name(self):
+        return self.MODEL_NAME
+        
+    def recommend_items(self, user_id, items_to_ignore=[], topn=10, verbose=False):
+        #Getting the top-1000 Content-based filtering recommendations
+        cb_recs_df = self.cb_rec_model.recommend_items(user_id, items_to_ignore=items_to_ignore, verbose=verbose,
+                                                           topn=1000).rename(columns={'recStrength': 'recStrengthCB'})
+        
+        #Getting the top-1000 Collaborative filtering recommendations
+        cf_recs_df = self.cf_rec_model.recommend_items(user_id, items_to_ignore=items_to_ignore, verbose=verbose, 
+                                                           topn=1000).rename(columns={'recStrength': 'recStrengthCF'})
+        
+        #Combining the results by contentId
+        recs_df = cb_recs_df.merge(cf_recs_df,
+                                   how = 'inner', 
+                                   left_on = 'contentId', 
+                                   right_on = 'contentId')
+        
+        #Computing a hybrid recommendation score based on CF and CB scores
+        recs_df['recStrengthHybrid'] = recs_df['recStrengthCB'] * recs_df['recStrengthCF']
+        
+        #Sorting recommendations by hybrid score
+        recommendations_df = recs_df.sort_values('recStrengthHybrid', ascending=False).head(topn)
+
+        if verbose:
+            if self.items_df is None:
+                raise Exception('"items_df" is required in verbose mode')
+
+            recommendations_df = recommendations_df.merge(self.items_df, how = 'left', 
+                                                          left_on = 'contentId', 
+                                                          right_on = 'contentId')[['recStrengthHybrid', 'contentId', 'title', 'url', 'lang']]
+
+
+        return recommendations_df
+    
+hybrid_recommender_model = HybridRecommender(content_based_recommender_model, cf_recommender_model, articles_df)
+
+
+'''Evaluating hybrid model'''
+#print('Evaluating Hybrid model...')
+hybrid_global_metrics, hybrid_detailed_results_df = model_evaluator.evaluate_model(hybrid_recommender_model)
+#print('\nGlobal metrics:\n%s' % hybrid_global_metrics)
+#hybrid_detailed_results_df.head(10)
+
+'''Comparing the methods'''
+global_metrics_df = pd.DataFrame([pop_global_metrics, cf_global_metrics, cb_global_metrics, hybrid_global_metrics]) \
+                        .set_index('modelName')
+#global_metrics_df
+#%matplotlib inline
+
+'''plot of comparison'''
+'''
+ax = global_metrics_df.transpose().plot(kind='bar', figsize=(15,8))
+for p in ax.patches:
+    ax.annotate("%.3f" % p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+'''
+
+'''Testing'''
+
+def inspect_interactions(person_id, test_set=True):
+    if test_set:
+        interactions_df = interactions_test_indexed_df
+    else:
+        interactions_df = interactions_train_indexed_df
+    return interactions_df.loc[person_id].merge(articles_df, how = 'left', 
+                                                      left_on = 'contentId', 
+                                                      right_on = 'contentId') \
+                          .sort_values('eventStrength', ascending = False)[['eventStrength', 
+                                                                          'contentId',
+                                                                          'title', 'url', 'lang']]
+
+#Testing for a user
+                          
+ans =inspect_interactions(-1479311724257856983, test_set=False).head(20)
+
+hybrid_result_ans=hybrid_recommender_model.recommend_items(-1479311724257856983, topn=20, verbose=True)
+
+
+
+
+
